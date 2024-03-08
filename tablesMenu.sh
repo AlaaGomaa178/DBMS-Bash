77aa1bc9205cee0b5fd8aeeea4e563bc95d8ecb7
AlaaGomaa178 <alaa.gomaa178@gmail.com>
2024-03-08 04:23:21 +0200

    sourced the list tables in the tables menu script

--- tablesMenu.sh
+++ tablesMenu.sh
@@ -4,7 +4,7 @@ tables_menu() {
     
     PS3="Enter your choice: " # ==> change it based on menu 
   
-    select Table in "Enter [C] to Create Table" s"Enter [L] to List Tables" "Enter [D] to Drop Table" "Enter [I] to Insert into Table" "Enter [S] to Select from Table" "Enter [R] to Delete from Table" "Enter [U] to Update Table" "Enter [Q] to Exit"
+    select Table in "Enter [C] to Create Table" "Enter [L] to List Tables" "Enter [D] to Drop Table" "Enter [I] to Insert into Table" "Enter [S] to Select from Table" "Enter [R] to Delete from Table" "Enter [U] to Update Table" "Enter [Q] to Exit"
     do
         case $REPLY in
             "C" | "c" ) 
@@ -12,6 +12,7 @@ tables_menu() {
                 createTable $DB_name
                 ;;
             "L" | "l" ) 
+                source ./listTables.sh
                 listTables
                 ;;
             "D" | "d" )
