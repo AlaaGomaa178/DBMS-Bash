ee68388cb7597c30b7892639beafa76f47ba9b1c
AlaaGomaa178 <alaa.gomaa178@gmail.com>
2024-03-08 03:40:45 +0200

    added Tables Menu script file that includes the menu after connecting to a specific DB

---
+++ tablesMenu.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+tables_menu() {
+    # Prompt the user to enter their choice
+    
+    PS3="Enter your choice: " # ==> change it based on menu 
+  
+    select Table in "Enter [C] to Create Table" s"Enter [L] to List Tables" "Enter [D] to Drop Table" "Enter [I] to Insert into Table" "Enter [S] to Select from Table" "Enter [R] to Delete from Table" "Enter [U] to Update Table" "Enter [Q] to Exit"
+    do
+        case $REPLY in
+            "C" | "c" ) 
+                source ./createTable.sh
+                createTable $DB_name
+                ;;
+            "L" | "l" ) 
+                listTables
+                ;;
+            "D" | "d" )
+                dropTable
+                ;;
+            "I" | "i" ) 
+                source ./insertIntoTable.sh
+                insertIntoTable 
+                ;;
+            "S" | "s" ) 
+                selectfromtable
+                ;;
+            "R" | "r" ) 
+                deletefromtable
+                ;;
+            "U" | "u" ) 
+                updatetable
+                ;;
+            "Q" | "q" ) # exit
+                source ./DBMS_Bash.sh
+                main_menu
+                ;;
+            * )
+                echo "Error: Invalid Choice"
+                ;;
+        esac
+    done
+}
+
+# Call the function
+tables_menu
