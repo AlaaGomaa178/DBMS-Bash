f31ae3b674344aa3a6985b2a31b30c60139bcb3a
AlaaGomaa178 <alaa.gomaa178@gmail.com>
2024-03-09 16:26:50 +0200

    modified the select from table script to include the select specific row option where the user will be able to select data based on the data of a specif row

--- selectTables.sh
+++ selectTables.sh
@@ -31,6 +31,79 @@ selectFromTable() {
                             cat "$db_name/$table_name"
                             ;;
                         
+
+                        "SelectSpecificRow")
+                            # Loop to allow user to select rows based on specific column values
+                            continueFlag=true
+                            
+                            while [[ $continueFlag == true ]]; 
+                            do
+                                # Read metadata to get column names and positions
+                                source "$db_name/$table_name.meta"
+                                
+                                # Associative array to store column positions
+                                declare -A col_positions  
+                                
+                                IFS=':' read -r -a col_names <<< "$COL_NAMES"
+                                
+                                for (( i=0; i<${#col_names[@]}; i++ )); 
+                                do
+                                    col_positions[${col_names[i]}]=$((i+1))
+                                done
+                                
+                                # Prompt user to select a column
+                                echo "Please select the column:"
+                                
+                                select cname in "${col_names[@]}"; 
+                                do
+                                    if [[ -n $cname ]]; 
+                                    then
+                                        # Check if the selected column exists
+                                        if [[ -n ${col_positions[$cname]} ]]; 
+                                        then
+                                            ColumnPosition=${col_positions[$cname]}
+                                            
+                                            while true; 
+                                            do
+                                                
+                                                # Prompt user to enter the value to select rows by
+                                                echo "Enter the value of '$cname' to select rows by it:"
+                                                read value
+                                                
+                                                # Check if the value exists in the selected column and print matching rows
+                                                awk -F':' -v col="$ColumnPosition" -v val="$value" '$col == val { print $0 }' "$db_name/$table_name" | 
+                                                
+                                                while IFS= read -r line; 
+                                                do
+                                                    if [[ ! -z $line ]]; then
+                                                        echo "$line"
+                                                    else
+                                                        echo "Your condition didn't match any rows."
+                                                    fi
+                                                done
+                                                break
+                                            done
+                                        else
+                                            echo "Invalid input! This column does not exist."
+                                        fi
+                                        break
+                                    else
+                                        echo "Invalid choice!"
+                                    fi
+                                done
+                                
+                                # Ask user if they want to select another row
+                                echo "Do you want to select another row? Enter 'y' to continue or any other key to cancel: "
+                                read answer
+
+                                if [[ $answer == "y" | "Y" ]]; 
+                                then
+                                    continueFlag=true
+                                else
+                                    break
+                                fi
+                            done
+                            ;;
 }
     
 
