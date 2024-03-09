4ffca7d1a54ab77d284888b6bbfc639243c7b59e
lina elduhemy <lina.elduhemy@gmail.com>
2024-03-10 01:00:11 +0200

    update tables with the new values

--- updateTable.sh
+++ updateTable.sh
@@ -8,18 +8,6 @@ update_table(){
     # Check if the table file exists in the selected database
     if [[ -f "$db_name/$table_name" && -f "$db_name/$table_name.meta" ]]; 
     then
-        #col_names=$(head -n 1 "$db_name/$table_name.meta" | tr ':' '\n')
-        #col_names=$(head -n 1 "$db_name/$table_name.meta" | sed 's/COL_NAMES=//' | tr ':' '\n')
-        #col_names=$(head -n 1 "$db_name/$table_name.meta" | sed 's/COL_NAMES=//' | tr ':' ' ')
-        #col_names=$(head -n 1 "$db_name/$table_name.meta" | sed 's/COL_NAMES="//;s/"$//')
-        # col_names=$(head -n 1 "$db_name/$table_name.meta" | sed 's/COL_NAMES="//;s/"$//' | tr ':' ' ')
-        # echo $col_names
-
-        # echo "Printing each element of the list:"
-        # for ((i=1 ;i<= ${#col_names[@]}; i++)) ;do
-        #     echo "$i) ${col_names[i]}"
-        # done
-
         source "$db_name/$table_name.meta"
         IFS=":" read -r -a column_names_array <<< "$COL_NAMES"
         for ((i = 1; i <= ${#column_names_array[@]}; i++)); 
@@ -28,20 +16,50 @@ update_table(){
                 done
 
         while true; do
-            read -rp "enter the column no. : " num
-            if [[ $num -lt 1 ]] || [[ $num -gt ${#column_names_array[@]} ]];
+            read -rp "enter the column no. to edit: " edit_column_num
+            if [[ $edit_column_num -lt 1 ]] || [[ $edit_column_num -gt ${#column_names_array[@]} ]];
             then 
                 echo "please enter a valid number"
             else 
                 echo "correct answer"
-                field=$((num - 1))
-                echo $field
                 break
             fi
         done
+        
+        # Prompt the user to enter the new value for the selected column
+        read -rp "Enter the new value for ${column_names_array[edit_column_num - 1]}: " new_value
 
-        # second_index=$(echo "$col_names" | awk '{print $2}')
-        # echo "The second index of col_names is: $second_index"
+        # Prompt the user to choose the column for the condition
+        read -rp "Enter the number of the column for the condition: " condition_column_num
+
+        # Prompt the user to enter the condition value for the selected column
+        read -rp "Enter the condition value for ${column_names_array[condition_column_num - 1]}: " condition_value
+        
+        #this works on when user select address column and enter new_value so it will update all addresses= condition_value
+        # sed -i "s/$condition_value/$new_value/g" $db_name/$table_name
+
+        # Check if the value exists in the selected column and print matching rows
+        # awk -F':' -v col="$condition_column_num" -v val="$condition_value" '$col == val { print $0 }' "$db_name/$table_name" | 
+        # while IFS= read -r line; 
+        #     do
+        #         if [[ ! -z $line ]]; then
+        #             echo "$line"
+        #             sed -i "s/$condition_value/$new_value/g" $db_name/$table_name 
+        #         else
+        #             echo "Your condition didn't match any rows."
+        #         fi
+        #     done
+
+        # Update the corresponding value in the data file based on the condition
+        awk -F':' -v edit_col="$edit_column_num" -v new_val="$new_value" -v cond_col="$condition_column_num" -v cond_val="$condition_value" '
+            BEGIN {OFS=":"}
+            {
+                if ($cond_col == cond_val) {
+                    $edit_col = new_val
+                }
+                print $0
+            }
+        ' "$db_name/$table_name" > "$db_name/$table_name.tmp" && mv "$db_name/$table_name.tmp" "$db_name/$table_name"
 
         source ./tablesMenu.sh
                 tables_menu
