467d21a2ddef9485360e5629d7833af50d82a378
lina elduhemy <lina.elduhemy@gmail.com>
2024-03-10 05:01:48 +0200

    editted DataBase name validaion

--- connecttoDB.sh
+++ connecttoDB.sh
@@ -5,6 +5,12 @@ selected_db=""
 
 connecttoDB(){
     read -p " >   Enter DB Name : " DB_name
+    allowed_pattern='^[A-Za-z0-9]+$'
+    while [[ ! $DB_name =~ $allowed_pattern || $DB_name =~ ^[0-9] ]]
+    do
+        echo  "      invalid name      "
+        read -p "$ >   Enter DB Name : " DB_name
+    done
     if [ -d "$DB_name" ]; 
     then
         # Store the selected database name in a global variable
