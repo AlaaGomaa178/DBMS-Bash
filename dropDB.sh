dca81e48189cb025964d70af293e0a764af9e315
lina elduhemy <lina.elduhemy@gmail.com>
2024-03-10 04:40:45 +0200

    editted DataBase name validaion

--- dropDB.sh
+++ dropDB.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
 dropDB () {
-    read -p "Enter DB you want to drop : " DB_name
+    while true; do
+        read -rp "Enter DB you want to drop: " DB_name
+        # Check if the input is empty or contains spaces
+        if [[ -z "$DB_name" ]]; then
+            echo "Input cannot be empty."
+        elif [[ "$DB_name" =~ [[:space:]] || ! "$DB_name" =~ ^[a-zA-Z0-9_]+$ ]]; then
+            echo "Input cannot contain spaces or special characters."
+        else
+            break  # Exit the loop if input is valid
+        fi
+    done
     if [ -d $DB_name ];
     then
         rm -r $DB_name
