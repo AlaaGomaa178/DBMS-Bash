87c3076a19f58227b6500947509075865dc1a2d3
AlaaGomaa178 <alaa.gomaa178@gmail.com>
2024-03-10 20:15:07 +0200

    Handled all the validations like the pk entry or the data type in the update

--- updateTable.sh
+++ updateTable.sh
@@ -3,69 +3,154 @@
 update_table(){
     local db_name=$selected_db
     
-    read -rp "Enter the name of the table to update: " table_name
-    
-    # Check if the table file exists in the selected database
-    if [[ -f "$db_name/$table_name" && -f "$db_name/$table_name.meta" ]]; 
-    then
-        source "$db_name/$table_name.meta"
-        IFS=":" read -r -a column_names_array <<< "$COL_NAMES"
-        for ((i = 1; i <= ${#column_names_array[@]}; i++)); 
-                do
-                    echo "$i) ${column_names_array[i-1]}"
-                done
+    while true; do
+        read -rp "Enter the name of the table to update: " table_name
+        
+        # Check if the table file exists in the selected database
+        if [[ -f "$db_name/$table_name" && -f "$db_name/$table_name.meta" ]]; 
+        then
+            source "$db_name/$table_name.meta"
+            IFS=":" read -r -a column_names_array <<< "$COL_NAMES"
+            IFS=":" read -r -a column_data_types_array <<< "$COL_DATATYPES"  # Add this line to read column data types
+            IFS=":" read -r -a column_PK_array <<< "$COL_PK"  # Add this line to read column PK metadata
+            
+            # Print column numbers and names for user selection
+            for ((i = 1; i <= ${#column_names_array[@]}; i++)); 
+            do
+                echo "$i) ${column_names_array[i-1]}"
+            done
 
-        while true; do
-            read -rp "enter the column no. to edit: " edit_column_num
-            if [[ $edit_column_num -lt 1 ]] || [[ $edit_column_num -gt ${#column_names_array[@]} ]];
-            then 
-                echo "please enter a valid number"
-            else 
-                echo "correct answer"
-                break
+            while true; do
+                read -rp "Enter the column number to edit: " edit_column_num
+                if [[ $edit_column_num -lt 1 ]] || [[ $edit_column_num -gt ${#column_names_array[@]} ]];
+                then 
+                    echo "Invalid column number. Please enter a valid number."
+                else 
+                    break
+                fi
+            done
+
+            # Prompt the user to enter the new value for the selected column
+            while true; do
+                read -rp "Enter the new value for ${column_names_array[edit_column_num - 1]}: " new_value
+                
+                # Validate new value based on column data type
+                case ${column_data_types_array[edit_column_num - 1]} in
+                    "Integer")
+                        if ! [[ $new_value =~ ^[0-9]+$ ]]; then
+                            echo "Invalid input! Please enter an integer for ${column_names_array[edit_column_num - 1]}."
+                        else
+                            break
+                        fi
+                        ;;
+                    "String")
+                        if [[ -z $new_value || $new_value == *:* ]]; then
+                            echo "Invalid input! Please enter a non-empty string without ':' for ${column_names_array[edit_column_num - 1]}."
+                        else
+                            break
+                        fi
+                        ;;
+                    *)
+                        echo "Unsupported data type: ${column_data_types_array[edit_column_num - 1]}"
+                        ;;
+                esac
+            done
+
+            # Find the index of the PK column
+            PK_index=-1
+            for ((i = 0; i < ${#column_names_array[@]}; i++)); 
+            do
+                if [[ ${column_PK_array[i]} == "yes" ]]; 
+                then
+                    PK_index=$i
+                    break
+                fi
+            done
+
+            # Check if a primary key column exists
+            if [[ $PK_index == -1 ]]; 
+            then
+                echo "No primary key column found in the table metadata."
+                return
             fi
-        done
-        
-        # Prompt the user to enter the new value for the selected column
-        read -rp "Enter the new value for ${column_names_array[edit_column_num - 1]}: " new_value
 
-        # Prompt the user to choose the column for the condition
-        read -rp "Enter the number of the column for the condition: " condition_column_num
+            # Check if the edited column is the primary key
+            if [[ $edit_column_num -eq $((PK_index + 1)) ]]; then
+                # Validate condition value uniqueness for PK column
+                if grep -q "^$new_value:" "$db_name/$table_name"; 
+                then
+                    echo "Primary key '$new_value' already exists. Please enter a different value for the primary key."
+                    continue
+                fi
+            fi
 
-        # Prompt the user to enter the condition value for the selected column
-        read -rp "Enter the condition value for ${column_names_array[condition_column_num - 1]}: " condition_value
-        
-        #this works on when user select address column and enter new_value so it will update all addresses= condition_value
-        # sed -i "s/$condition_value/$new_value/g" $db_name/$table_name
+            # Prompt the user to choose the column for the condition
+            while true; do
+                read -rp "Enter the number of the column for the condition: " condition_column_num
+                if [[ $condition_column_num -lt 1 ]] || [[ $condition_column_num -gt ${#column_names_array[@]} ]];
+                then 
+                    echo "Invalid column number for the condition. Please enter a valid number."
+                else
+                    break
+                fi
+            done
 
-        # Check if the value exists in the selected column and print matching rows
-        # awk -F':' -v col="$condition_column_num" -v val="$condition_value" '$col == val { print $0 }' "$db_name/$table_name" | 
-        # while IFS= read -r line; 
-        #     do
-        #         if [[ ! -z $line ]]; then
-        #             echo "$line"
-        #             sed -i "s/$condition_value/$new_value/g" $db_name/$table_name 
-        #         else
-        #             echo "Your condition didn't match any rows."
-        #         fi
-        #     done
+            # Prompt the user to enter the condition value for the selected column
+            while true; do
+                read -rp "Enter the condition value for ${column_names_array[condition_column_num - 1]}: " condition_value
+                
+                # Validate condition value based on column data type
+                case ${column_data_types_array[condition_column_num - 1]} in
+                    "Integer")
+                        if ! [[ $condition_value =~ ^[0-9]+$ ]]; then
+                            echo "Invalid input! Please enter an integer for the condition value."
+                        else
+                            break
+                        fi
+                        ;;
+                    "String")
+                        if [[ -z $condition_value || $condition_value == *:* ]]; then
+                            echo "Invalid input! Please enter a non-empty string without ':' for the condition value."
+                        else
+                            break
+                        fi
+                        ;;
+                    *)
+                        echo "Unsupported data type for condition column: ${column_data_types_array[condition_column_num - 1]}"
+                        ;;
+                esac
+            done
 
-        # Update the corresponding value in the data file based on the condition
-        awk -F':' -v edit_col="$edit_column_num" -v new_val="$new_value" -v cond_col="$condition_column_num" -v cond_val="$condition_value" '
-            BEGIN {OFS=":"}
-            {
-                if ($cond_col == cond_val) {
-                    $edit_col = new_val
+            # Check if the condition value exists in the specified column
+            if ! grep -q "^$condition_value:" "$db_name/$table_name"; 
+            then
+                echo "No value equal to the condition value ('$condition_value') was found in the specified column. Please enter a valid condition value."
+                continue
+            fi
+
+            # Update the corresponding value in the data file based on the condition
+            awk -F':' -v edit_col="$edit_column_num" -v new_val="$new_value" -v cond_col="$condition_column_num" -v cond_val="$condition_value" '
+                BEGIN {OFS=":"}
+                {
+                    if ($cond_col == cond_val) {
+                        $edit_col = new_val
+                    }
+                    print $0
                 }
-                print $0
-            }
-        ' "$db_name/$table_name" > "$db_name/$table_name.tmp" && mv "$db_name/$table_name.tmp" "$db_name/$table_name"
-#TEMP file helps integrity of the data, especially in case of unexpected errors during the update process, Backup.
-#Concurrency: If multiple processes or users are accessing the same file, writing to a temporary file ensures that other processes/users don't encounter inconsistent or corrupted data while the update is in progress.
+            ' "$db_name/$table_name" > "$db_name/$table_name.tmp" && mv "$db_name/$table_name.tmp" "$db_name/$table_name"
 
-        source ./tablesMenu.sh
-                tables_menu
-    else
-        echo "Table '$table_name' does not exist."
-    fi
+            echo "========================="
+            echo "Data updated successfully."
+            echo "========================="
+
+            # Return to the tables menu
+            source ./tablesMenu.sh
+            tables_menu
+            break
+        else
+            echo "Table '$table_name' does not exist."
+        fi
+    done
 }
+
+update_table
