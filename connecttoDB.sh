9efcd4d3328f45d8e74a55978e15ac4473f317c2
AlaaGomaa178 <alaa.gomaa178@gmail.com>
2024-03-10 18:06:51 +0200

    Modified some messages that appear to the user

--- connecttoDB.sh
+++ connecttoDB.sh
@@ -4,27 +4,39 @@
 selected_db=""
 
 connecttoDB(){
+    
+    echo
     read -p " >   Enter DB Name : " DB_name
+    echo
+    
     allowed_pattern='^[A-Za-z0-9]+$'
+    
     while [[ ! $DB_name =~ $allowed_pattern || $DB_name =~ ^[0-9] ]]
     do
-        echo  "      invalid name      "
+        echo
+        echo  "      Invalid name! DB name must start with a letter and contain only letters and numbers!      "
+        echo
+        
         read -p "$ >   Enter DB Name : " DB_name
     done
     if [ -d "$DB_name" ]; 
     then
         # Store the selected database name in a global variable
         export selected_db=$DB_name
+
         echo
         echo "                        ~~~ Selected DB: $selected_db ~~~" 
         echo 
+
         # Navigate to the tables menu
         source ./tablesMenu.sh
-        tables_menu       
+        tables_menu     
+
     else
-        echo "DB doesn't exist"
+        echo " !!! DB doesn't exist !!!"
         source ./DBMS_Bash.sh
         main_menu
+        
     fi
 }
 
--- createDB.sh
+++ createDB.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
+
 shopt -s extglob
-    export LC_COLLATE=C
+export LC_COLLATE=C
 
 
-	createDB () {
+createDB () {
     read -p " >   Enter DB Name : " DB_name
 
     allowed_pattern='^[A-Za-z0-9]+$'
     while [[ ! $DB_name =~ $allowed_pattern || $DB_name =~ ^[0-9] ]]
     do
+        echo
         echo  "      Invalid name! DB name must start with a letter and contain only letters and numbers!      "
+        echo
+
         read -p "$ >   Enter DB Name : " DB_name
     done
 
